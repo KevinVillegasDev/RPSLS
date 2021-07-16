e2c4377cbea795ebeb03c3973464e6513330f166
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-07-16 13:37:02 -0700

    created AI class and passed in player inheritance. created get_gesture method that will let AI select random gesture from list and prints out selection.

--- ai.py
+++ ai.py
@@ -1,1 +1,14 @@
-class AI:
+from player import Player
+import random
+
+class AI(Player):
+    def __init__(self):
+        self.name = "Computer"
+        super().__init__()
+    
+    def get_gesture(self):
+        self.choice = self.gesture[random.randint(1,5)]
+        print("The computer selects " + self.choice)
+        
+        
+        
