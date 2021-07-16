e7b63a0d9eaf4c55064e523ef602dfa13ede3b8e
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-07-16 13:44:08 -0700

    Created game class. Added empty methods to start setting file up and created display welcome method with a print statement.

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
+from ai import AI
+from human import Human
+
+
 class Game:
+    def __init__(self): #need to create option for two players or one player
+        pass
+    
+    def display_welcome(self):
+        print("Welcome to Rock, Paper, Scissors but with a twist! Introducing spock and lizard!")
+        
+    
+    def run_game(self):
+        self.display_welcome()
+    
+    def multiplayer(self):
+        pass
+    
+    
