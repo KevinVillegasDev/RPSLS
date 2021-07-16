34be452faecf16ba9a1ce4b7a9282b2f611b3fd7
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-07-16 11:51:04 -0700

    Added some items to algorithm

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Step 1- Start the game
 # Step 2- Display rules of the game
 # Step 3- Ask user if game will be played in 1 player vs AI or 2 player
-# Step 4- First player will choose a gesture
-# Step 5- Second player will choose a gesture
-# Step 6- Both gestures will be displayed
-# Step 7- Winning gesture will be determined and round winner displayed
-# Step 8- Prompt players for new gestures for second round
-# Step 9- Repeat step 4, 5, 6, 7 two more times
-# Step 10- If any player wins first two rounds, display match winner and skip round 3
+# Step 4- Display a list of gesture options
+# Step 5- First player will choose a gesture
+# Step 6- Second player will choose a gesture
+# Step 7- Both gestures will be displayed
+# Step 8- Winning gesture will be determined and round winner displayed and track the score (check for ties)
+# Step 9- Prompt players for new gestures for second round
+# Step 10- Repeat step 4, 5, 6, 7 two more times
+# Step 11- If any player wins first two rounds, display match winner and skip round 3
+
+
 
 
