e40ba3b64f3c2e8705d11198686805ad79851dda
Paul-Longo <imanazarite88@gmail.com>
2021-07-16 17:05:01 -0400

    Added 2 more print statements to display rules and Lets get started. added method games_rounds_score and display winner. added display winner to run game method

--- game.py
+++ game.py
@@ -3,17 +3,23 @@ from human import Human
 
 
 class Game:
-    def __init__(self): #need to create option for two players or one player
+    def __init__(self):  # need to create option for two players or one player
         pass
-    
+
     def display_welcome(self):
         print("Welcome to Rock, Paper, Scissors but with a twist! Introducing spock and lizard!")
-        
-    
+        print("Rules: Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard ")
+        print("Let's Get Started!")
+
     def run_game(self):
         self.display_welcome()
-    
+        self.display_winner()
+
     def multiplayer(self):
         pass
-    
-    
+
+    def game_rounds_score():
+        pass
+
+    def display_winner():
+        pass
