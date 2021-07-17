a9dff13a39a90a829b076623527321c3b0f4f8e5
Paul-Longo <imanazarite88@gmail.com>
2021-07-17 14:51:29 -0400

    updated multiplayer and added a method for single player. added p1 and p2 scores set to zero.updated compare scores method

--- game.py
+++ game.py
@@ -1,20 +1,24 @@
 from ai import AI
 from human import Human
 
+# added player scores to be set to 0 to allow for calculating wins.
+player_one_score = 0
+player_two_score = 0
+
 
 class Game:
-    def __init__(self):  # need to create option for two players or one player
+    def __init__(self):
         self.player_one = Human()
-        self.player_two = AI()
+        self.player_two = ""  # create option for player two to be a human or AI
 
     def display_welcome(self):
         print("Welcome to Rock, Paper, Scissors but with a twist! Now Introducing Spock and Lizard!")
         print("Rules:\n Rock crushes Scissors \n Scissors cuts Paper \n Paper covers Rock \n Rock crushes Lizard \n Lizard poisons Spock \n Spock smashes Scissors \n Scissors decapitates Lizard \n Best of three rounds wins! \n")
         print("Let's Get Started!")
 
-    def multiplayer(self):
+    def game_mode(self):
         user_input = input(
-            "Press 1: Player vs CPU \n Press 2: Player vs Player\n")
+            "Press 1: Player vs CPU\nPress 2: Player vs Player\n")
         if user_input == 1:
             self.player_one = Human()
             self.player_two = AI()
@@ -23,18 +27,34 @@ class Game:
             self.player_one = Human()
             self.player_two = Human()
 
+# TODO: finish comparing player one and player two gestures/round.
+    def single_player(self):
+        global player_one_score
+        global player_two_score
+        self.player_one_turn =
+        self.player_two_turn =
+        pass
+# TODO: finish comparing player one and player two gestures/round.
+
+    def multiplayer(self):
+        global player_one_score
+        global player_two_score
+        self.player_one_turn =
+        self.player_two_turn =
+        if self.player_one == self.player_two:
+            print("Tie")
+
     def game_rounds(self):
         print("You will need to win 2 rounds to be the overall Winner!")
-        number = 2
-        if number < 2:
-            print("Almost there! You just need to win 2 rounds.\n Keep going!")
-            return self.game_rounds
-        # Add algorithm to calculate what round is being played.
-        pass
 
     def compare_scores(self):
-        # Algorithm to calculate the total number of wins each player has and compare them to see who is winning/won.
-        pass
+        while player_one_score < 2 and player_two_score < 2:
+            if player_one_score == 2:
+                print("WINNER! WINNER! CHICKEN DINNER!")
+            else:
+                print("Player ... Wins!")
+                return self.game_rounds
+            pass
 
     def display_winner(self):
         # if player_one wins is larger than player two wins.
@@ -44,5 +64,7 @@ class Game:
 
     def run_game(self):
         self.display_welcome()
-        self.multiplayer()
+        self.game_mode()
+        self.game_rounds()
         self.display_winner()
+        self.multiplayer()
