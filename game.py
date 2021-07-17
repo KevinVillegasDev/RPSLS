17b3a5a492563a8967a13cde6a8000d2886d30db
Paul-Longo <imanazarite88@gmail.com>
2021-07-17 10:56:28 -0400

    added user input and if/elif/else statement in multiplayer. moved run game method to the end code line. addend comments to work on

--- game.py
+++ game.py
@@ -4,22 +4,39 @@ from human import Human
 
 class Game:
     def __init__(self):  # need to create option for two players or one player
-        pass
+        self.player_one = Human()
+        self.player_two = AI()
+        self.player_three = Human()
 
     def display_welcome(self):
-        print("Welcome to Rock, Paper, Scissors but with a twist! Introducing spock and lizard!")
+        print("Welcome to Rock, Paper, Scissors but with a twist! Now Introducing Spock and Lizard!")
         print("Rules:\n Rock crushes Scissors \n Scissors cuts Paper \n Paper covers Rock \n Rock crushes Lizard \n Lizard poisons Spock \n Spock smashes Scissors \n Scissors decapitates Lizard \n")
         print("Let's Get Started!")
 
-    def run_game(self):
-        self.display_welcome()
-        self.display_winner()
-
     def multiplayer(self):
+        user_input = input(
+            "Press 1: Player vs CPU \n Press 2: Player vs Player ")
+        if user_input == 1:
+            pass
+        # set player two from AI to human.
+        elif user_input == 2:
+            pass
+
+    def game_rounds():
+        # Add algorithm to calculate what round is being played.
         pass
 
-    def game_rounds_score():
+    def compare_scores():
+        # Algorithm to calculate the total number of wins each player has and compare them to see who is winning/won.
         pass
 
     def display_winner():
+        # if player_one wins is larger than player two wins.
+        # then player one wins
+        # else if player two is the winner.
         pass
+
+    def run_game(self):
+        self.display_welcome()
+        self.multiplayer()
+        self.display_winner()
