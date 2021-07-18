0a729eacd5885c3d0aa20fa3e749b2479fcec5d9
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-07-18 14:11:55 -0700

    added name attribute

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 class Player:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.gesture = self.create_gesture()
         self.score = 0
 
@@ -8,17 +9,17 @@ class Player:
         return self.gesture
 
     def select_gesture(self):
-        self.choose_gesture = int(input(
-            "What gesture do you choose? 1 = rock, 2 = paper, 3 = scissors, 4 = spock, 5 = lizard \n"))
-        if self.choose_gesture == 1:
+        self.choose_gesture = input(
+            "What gesture do you choose? 1 = rock, 2 = paper, 3 = scissors, 4 = spock, 5 = lizard \n")
+        if self.choose_gesture == "1":
             self.choose_gesture = "rock"
-        elif self.choose_gesture == 2:
+        elif self.choose_gesture == "2":
             self.choose_gesture = "paper"
-        elif self.choose_gesture == 3:
+        elif self.choose_gesture == "3":
             self.choose_gesture = "scissors"
-        elif self.choose_gesture == 4:
+        elif self.choose_gesture == "4":
             self.choose_gesture = "spock"
-        elif self.choose_gesture == 5:
+        elif self.choose_gesture == "5":
             self.choose_gesture = "lizard"
         print("Player has selected " + self.choose_gesture)
         
