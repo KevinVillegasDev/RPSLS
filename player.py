163f5f901e1e1e8c24048eec94bb987a447efb51
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-07-19 12:33:12 -0700

    Added input validation loop in choose gesture class.

--- player.py
+++ player.py
@@ -8,19 +8,28 @@ class Player:
         self.gesture = ["rock", "paper", "scissors", "spock", "lizard"]
         return self.gesture
 
-    def select_gesture(self):
-        self.choose_gesture = input(
-            "What gesture do you choose? 1 = rock, 2 = paper, 3 = scissors, 4 = spock, 5 = lizard \n")
-        if self.choose_gesture == "1":
-            self.choose_gesture = "rock"
-        elif self.choose_gesture == "2":
-            self.choose_gesture = "paper"
-        elif self.choose_gesture == "3":
-            self.choose_gesture = "scissors"
-        elif self.choose_gesture == "4":
-            self.choose_gesture = "spock"
-        elif self.choose_gesture == "5":
-            self.choose_gesture = "lizard"
-        print("Player has selected " + self.choose_gesture)
-        return self.choose_gesture
-        
+    def choose_gesture(self):
+        isValid = False
+        while isValid == False:
+            self.chosen_gesture = input(
+                "What gesture do you choose? 1 = rock, 2 = paper, 3 = scissors, 4 = spock, 5 = lizard \n")
+            if self.chosen_gesture == "1":
+                self.chosen_gesture = "rock"
+                isValid = True
+            elif self.chosen_gesture == "2":
+                self.chosen_gesture = "paper"
+                isValid = True
+            elif self.chosen_gesture == "3":
+                self.chosen_gesture = "scissors"
+                isValid = True
+            elif self.chosen_gesture == "4":
+                self.chosen_gesture = "spock"
+                isValid = True
+            elif self.chosen_gesture == "5":
+                self.chosen_gesture = "lizard"
+                isValid = True
+            else:
+                print("Not a valid Selection")
+        print("Player has selected " + self.chosen_gesture)
+        return self.chosen_gesture
+    
